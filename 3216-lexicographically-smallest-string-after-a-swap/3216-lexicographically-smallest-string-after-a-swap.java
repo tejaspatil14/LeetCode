f0fa0cde27e6abd6ae387abb253b8363bc29fe88
TEJAS PATIL <134291831+tejaspatil14@users.noreply.github.com>
2024-08-12 15:23:06 +0530

    Time: 1 ms (100.00%), Space: 42.9 MB (52.39%) - LeetHub

--- 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.java
+++ 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.java
@@ -3,29 +3,19 @@ class Solution {
         char[] arr = s.toCharArray();
         int n = arr.length;
         
-        // Initialize the variables to track the minimum string after swaps
-        String minString = s;
+       
         
         // Try all possible adjacent swaps with the same parity
         for (int i = 0; i < n - 1; i++) {
-            if ((arr[i] % 2) == (arr[i + 1] % 2)) {
-                // Swap the characters
+            if ((arr[i] % 2) == (arr[i + 1] % 2) && arr[i]>arr[i+1]) {
+
                 char temp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;
-                
-                // Update the minimum string if the new string is smaller
-                String newString = new String(arr);
-                if (newString.compareTo(minString) < 0) {
-                    minString = newString;
-                }
-                
-                // Swap back to try other possibilities
-                arr[i + 1] = arr[i];
-                arr[i] = temp;
+                break;
             }
         }
         
-        return minString;
+        return new String (arr);
     }
 }
