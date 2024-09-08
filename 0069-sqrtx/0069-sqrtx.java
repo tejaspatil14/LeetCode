b2cae54d82cc77e0b3596f19dc8ec8b5c62f0aad
TEJAS PATIL <134291831+tejaspatil14@users.noreply.github.com>
2024-09-08 07:57:10 +0530

    Time: 1 ms (85.71%), Space: 41 MB (30.87%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,26 +1,21 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) return x;
-
-        int y = x;
-        while (true) {
-            y = y / 2;
-            if ((y * y) < 0 || (y * y) / y != y) continue; // handle int overflow
-            if ((y * y) > 0 && (y * y) <= x) break;
-        }
-
-        if (y * y == x) return y;
-
-        int j = x - (y * y);
-        while (true) {
-            j = j - (y + y + 1);
-            if (j >= 0) {
-                y = y + 1;
-            } else {
-                break;
+        long low=1,high=x;
+        int ans=0;
+        while(low<=high)
+        {
+            long mid=low+(high-low)/2;
+            if((mid*mid)>x)
+            {
+               
+                high=mid-1;
+            }
+            else
+            {
+                ans=(int)mid;
+                low=mid+1;
             }
         }
-
-        return y;
+        return ans;
     }
 }
